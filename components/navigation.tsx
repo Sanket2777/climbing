b6f632770a3a4cff53ef0fd2ebe6d82293327161
sanketbhandare911 <sanketbhandare277@gmail.com>
2025-10-03 13:17:24 +0530

    first commit

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -17,7 +17,7 @@ export function Navigation() {
           {/* Logo */}
           <Link href="/" className="flex items-center space-x-2 z-50 relative">
             <Image
-              src="/climbing.png"
+              src="classes/climbing.png"
               alt="Peak Climb Logo"
               width={150}
               height={150}
